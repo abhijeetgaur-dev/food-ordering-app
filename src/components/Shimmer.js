7840882a24e2ad4ed6d033bd42f7e4417e3e6e4c
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-08-21 13:27:12 +0530

    Shimmer Fix

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,15 +1,16 @@
- export const Shimmer = () =>{
-    return (
-        <div className="shimmer-container">
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-        </div>
-    )
-}
 
-// export default Shimmer
+ export const Shimmer = () =>{
+    return <>
+        <div className="flex flex-wrap justify-start gap-3 mt-0">
+            <div className="animate-pulse m-4 w-[200px] bg-[#4A5D5E] h-[200px] rounded-md shadow-gray-950 shadow-sm"> </div>
+            <div className="animate-pulse m-4 w-[200px] bg-[#4A5D5E] h-[200px] rounded-md shadow-gray-950 shadow-sm"> </div>
+            <div className="animate-pulse m-4 w-[200px] bg-[#4A5D5E] h-[200px] rounded-md shadow-gray-950 shadow-sm"> </div>
+            <div className="animate-pulse m-4 w-[200px] bg-[#4A5D5E] h-[200px] rounded-md shadow-gray-950 shadow-sm"> </div>
+            <div className="animate-pulse m-4 w-[200px] bg-[#4A5D5E] h-[200px] rounded-md shadow-gray-950 shadow-sm"> </div>
+            <div className="animate-pulse m-4 w-[200px] bg-[#4A5D5E] h-[200px] rounded-md shadow-gray-950 shadow-sm"> </div>
+            <div className="animate-pulse m-4 w-[200px] bg-[#4A5D5E] h-[200px] rounded-md shadow-gray-950 shadow-sm"> </div>
+            <div className="animate-pulse m-4 w-[200px] bg-[#4A5D5E] h-[200px] rounded-md shadow-gray-950 shadow-sm"> </div>
+            <div className="animate-pulse m-4 w-[200px] bg-[#4A5D5E] h-[200px] rounded-md shadow-gray-950 shadow-sm"> </div>
+        </div>
+    </>
+}

72b25c36182fc4bf01afe480aa64ea0729512892
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-08-21 10:17:58 +0530

    UI fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 const Footer = () => {
     return (
-      <div className="text-center bg-[#2c3232] fixed inset-x-0 bottom-0">
+      <div className="text-center bg-[#2c3232] fixed inset-x-0 bottom-0 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_-2px_4px_-1px_rgba(0,0,0,0.06)]">
           <div className="py-4">
           Created by Abhijeet Gaur
           <div className="py-2">
           <a href="https://www.linkedin.com/in/abhijeet-gaur-848754300/">LinkedIn</a> ·
           <a href="https://github.com/abhijeetgaur-dev">GitHub</a> · 
-          <a href="mailto:abhijeetgaur777@gmail.com">Contact</a> · 
+          <a href="mailto:abhijeetgaur.dev@gmail.com">Contact</a> · 
           <a href="https://drive.google.com/drive/folders/1I5mWLN7M5ukHls3ULKItJ3q2M0ejjlUc?usp=drive_link">Resume</a>
           </div>
       </div>
